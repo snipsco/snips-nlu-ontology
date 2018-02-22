5ff7ddfaf92763c81abf9bd799a7706a53e179f9
Adrien Ball <adrien.ball@snips.net>
2018-02-22 11:39:51 +0100

    Fix python tests

--- snips-nlu-ontology-ffi/platforms/snips-nlu-ontology-python/snips_nlu_ontology/tests/test_builtin_entity_parser.py
+++ snips-nlu-ontology-ffi/platforms/snips-nlu-ontology-python/snips_nlu_ontology/tests/test_builtin_entity_parser.py
@@ -6,31 +6,30 @@ from snips_nlu_ontology import BuiltinEntityParser
 
 
 class TestBuiltinEntityParser(unittest.TestCase):
-    def test_should_parser_without_scope(self):
+    def test_should_parse_without_scope(self):
         # Given
         parser = BuiltinEntityParser("en")
 
         # When
-        res = parser.parse("what will be the weather on Apr. 1st 2018 ?")
+        res = parser.parse("Raise to sixty two degrees celsius")
 
         # Then
         expected_result = [
             {
-                'value': 'on Apr. 1st 2018',
-                'range': {'start': 25, 'end': 41},
-                'entity': {
-                    'kind': 'InstantTime',
-                    'value': '2018-04-01 00:00:00 +02:00',
-                    'grain': 'Day',
-                    'precision': 'Exact'
+                "entity": {
+                    "kind": "Temperature",
+                    "unit": "celsius",
+                    "value": 62.0
                 },
-                'entity_kind': 'snips/datetime'
+                "entity_kind": "snips/temperature",
+                "range": {"end": 34, "start": 9},
+                "value": "sixty two degrees celsius"
             }
         ]
 
         self.assertListEqual(expected_result, res)
 
-    def test_should_parser_with_scope(self):
+    def test_should_parse_with_scope(self):
         # Given
         parser = BuiltinEntityParser("en")
         scope = ["snips/temperature", "snips/number"]
@@ -41,14 +40,14 @@ class TestBuiltinEntityParser(unittest.TestCase):
         # Then
         expected_result = [
             {
-                'entity': {
-                    'kind': 'Temperature',
-                    'unit': None,
-                    'value': 62.0
+                "entity": {
+                    "kind": "Temperature",
+                    "unit": None,
+                    "value": 62.0
                 },
-                'entity_kind': 'snips/temperature',
-                'range': {'end': 18, 'start': 9},
-                'value': 'sixty two'
+                "entity_kind": "snips/temperature",
+                "range": {"end": 18, "start": 9},
+                "value": "sixty two"
             }
         ]
 
