ddb761d78da6a6e8826b54247f5427d111d1581a
Kevin Lefevre <kevin.lefevre@snips.ai>
2018-03-01 11:53:18 +0100

    Update install script to build with and without default features

--- .travis/install.sh
+++ .travis/install.sh
@@ -7,4 +7,10 @@ export PATH="/usr/local/bin:$HOME/.cargo/bin:$PATH"
 export CARGO_TARGET_DIR="$TRAVIS_BUILD_DIR/snips-nlu-ontology-ffi/platforms/snips-nlu-ontology-python/snips-nlu-ontology-rs/target"
 
 perl -p -i -e "s/^snips-nlu-ontology-ffi = .*\$/snips-nlu-ontology-ffi = { path = \"..\/..\/..\" \}/g" */**/**/*/Cargo.toml
-cargo build --all  || die "Rust build failed"
+
+echo "Building with --no-default-features"
+cargo build --no-default-features || die "snips-nlu-ontology with --no-default-features failed"
+$(cd snips-nlu-ontology-ffi && cargo build --no-default-features) || die "snips-nlu-ontology-ffi with --no-default-features failed"
+
+echo "Building with default features"
+cargo build --all || die "Rust build failed"
