8e265c43002dfe7849b68e75c275241ab5434dd3
Kevin Lefevre <kevin.lefevre@snips.ai>
2018-03-12 17:38:46 +0100

    Move snips-nlu-ontology in a directory and remove workspace

--- update_version.sh
+++ update_version.sh
@@ -1,14 +1,12 @@
 #!/usr/bin/env bash
-source ./.travis/common.sh
-
 parseRustVersion() {
-    grep -w Cargo.toml -e '^version = ".*' | sed -- 's/version = "//g' | sed -- 's/"//g'
+    grep -w snips-nlu-ontology/Cargo.toml -e '^version = ".*' | sed -- 's/version = "//g' | sed -- 's/"//g'
 }
 
 NEW_VERSION=$(parseRustVersion)
 echo "Updating versions to version ${NEW_VERSION}"
-perl -p -i -e "s/^version = \".*\"\$/version = \"$NEW_VERSION\"/g" */Cargo.toml
-perl -p -i -e "s/^version = \".*\"\$/version = \"$NEW_VERSION\"/g" */**/**/*/Cargo.toml
-perl -p -i -e "s/https:\/\/github\.com\/snipsco\/snips-nlu-ontology\", tag = \".*\"/https:\/\/github\.com\/snipsco\/snips-nlu-ontology\", tag = \"$NEW_VERSION\"/g" snips-nlu-ontology-ffi/platforms/snips-nlu-ontology-python/snips-nlu-ontology-rs/Cargo.toml
-perl -p -i -e "s/^version = \".*\"\$/version = \"$NEW_VERSION\"/g" */**/*/build.gradle
-echo "$NEW_VERSION" > snips-nlu-ontology-ffi/platforms/snips-nlu-ontology-python/snips_nlu_ontology/__version__
+find . -name "Cargo.toml" -exec perl -p -i -e "s/^version = \".*\"$/version = \"$NEW_VERSION\"/g" {} \;
+find . -name "build.gradle" -exec perl -p -i -e "s/^version = \".*\"$/version = \"$NEW_VERSION\"/g" {} \;
+perl -p -i -e "s/snips-nlu-ontology\", tag = \".*\"/snips-nlu-ontology\", tag = \"$NEW_VERSION\"/g" \
+    platforms/snips-nlu-ontology-python/snips-nlu-ontology-rs/Cargo.toml
+echo "$NEW_VERSION" > platforms/snips-nlu-ontology-python/snips_nlu_ontology/__version__
