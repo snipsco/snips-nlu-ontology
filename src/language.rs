baaa57846104b853aadb07e30d5c512de0ab406b
ClemDoum <clement.doumouro@gmail.com>
2018-03-15 14:28:54 +0100

    Fix Japanse label

--- src/language.rs
+++ src/language.rs
@@ -59,7 +59,7 @@ impl Language {
             Language::ES => "Spanish",
             Language::KO => "Korean",
             Language::DE => "German",
-            Language::JA => "Japan",
+            Language::JA => "Japanese",
         }
     }
 }
