fde692e9d67759a5e9d349afdfa8e2cf90f3e078
Adrien Ball <adrien.ball@snips.net>
2018-04-10 15:11:42 +0200

    Add import in python package file

--- platforms/snips-nlu-ontology-python/snips_nlu_ontology/__init__.py
+++ platforms/snips-nlu-ontology-python/snips_nlu_ontology/__init__.py
@@ -2,4 +2,4 @@ from __future__ import absolute_import
 
 from snips_nlu_ontology.builtin_entities import (
     BuiltinEntityParser, get_all_languages, get_all_builtin_entities,
-    get_supported_entities, get_ontology_version)
+    get_supported_entities, get_ontology_version, get_builtin_entity_examples)
